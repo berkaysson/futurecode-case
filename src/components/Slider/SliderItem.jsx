d1b420cc4428557af16c60dfe2d8221c0806e197
berkaysson <berkaysonel85@gmail.com>
2024-05-17 23:35:39 +0300

    Update position of carousel caption

--- src/components/Slider/SliderItem.jsx
+++ src/components/Slider/SliderItem.jsx
@@ -4,7 +4,7 @@ const SliderItem = ({ imageSrc, imageAlt, title, caption }) => {
   return (
     <div className="carousel-item active">
       <img src={imageSrc} className="d-block w-100" alt={imageAlt} />
-      <div className="carousel-caption position-absolute d-inline-flex top-0 start-20">
+      <div className="carousel-caption position-absolute">
         <div className="d-flex flex-row align-items-center">
           <div className="carousel-caption-shape"></div>
           <div className="text-start carousel-caption-text">
