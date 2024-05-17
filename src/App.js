4d00dd56e67cbf2244ca4a7054d62ae16513320f
berkaysson <berkaysonel85@gmail.com>
2024-05-17 23:07:11 +0300

    Implement Slider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Menu from "./components/Menu/Menu";
+import Slider from "./components/Slider/Slider";
 
 function App() {
   return (
     <div className="">
       <Menu />
+      <Slider />
     </div>
   );
 }
