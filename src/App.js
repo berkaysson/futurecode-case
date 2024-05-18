ac023ad232651242eca5edbe6b3319f3d13564b9
berkaysson <berkaysonel85@gmail.com>
2024-05-18 20:42:26 +0300

    Implement MobileMenu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import { useEffect, useState } from "react";
 import Menu from "./components/Menu/Menu";
 import OurServices from "./components/OurServices/OurServices";
 import Slider from "./components/Slider/Slider";
+import MobileMenu from "./components/MobileMenu/MobileMenu";
 
 function App() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="">
-      <Menu />
+      {isMobile ?  <MobileMenu /> : <Menu />}
       <Slider />
       <OurServices />
     </div>
