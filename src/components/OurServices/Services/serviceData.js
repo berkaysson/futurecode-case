f5ed2bce97c72bc84c9b41da7f53da2137c2107a
berkaysson <berkaysonel85@gmail.com>
2024-05-18 13:49:01 +0300

    Create services object to render informations and images

---
+++ src/components/OurServices/Services/serviceData.js
@@ -0,0 +1,59 @@
+import KarayoluImg from "../../../assets/images/services/karayolu.png";
+import HavayoluImg from "../../../assets/images/services/havayolu.png";
+import DenizyoluImg from "../../../assets/images/services/denizyolu.png";
+import DemiryoluImg from "../../../assets/images/services/demiryolu.png";
+import ETicaretImg from "../../../assets/images/services/elojistik.png";
+import DepolamaImg from "../../../assets/images/services/depolama.png";
+import SprintImg from "../../../assets/images/services/sprint.png";
+import KontratImg from "../../../assets/images/services/kontrat.png";
+
+export const services = [
+  {
+    title: "Karayolu Taşımacılığı",
+    description: "Avrupa başta olmak üzere dünyadaki birçok noktaya karayolu taşımacılığı hizmeti sunmaktayız.",
+    imgSrc: KarayoluImg,
+    imgAlt: "Karayolu Taşımacılığı",
+  },
+  {
+    title: "Havayolu Taşımacılığı",
+    description: "Dünyanın önemli şehirlerine havayolu taşımacılığı hizmetimizle ulaşabilirsiniz.",
+    imgSrc: HavayoluImg,
+    imgAlt: "Havayolu Taşımacılığı",
+  },
+  {
+    title: "Denizyolu Taşımacılığı",
+    description: "Dünyadaki birçok limana ulaşabileceğiniz denizyolu taşımacılığı hizmeti sunmaktayız.",
+    imgSrc: DenizyoluImg,
+    imgAlt: "Denizyolu Taşımacılığı",
+  },
+  {
+    title: "Demiryolu Taşımacılığı",
+    description: "Dünyadaki birçok önemli merkeze demir yolu taşımacılığı hizmeti sunmaktayız.",
+    imgSrc: DemiryoluImg,
+    imgAlt: "Demiryolu Taşımacılığı",
+  },
+  {
+    title: "E-Ticaret Lojistiği",
+    description: "E-ticaret uygulamalarınızda, Enco olarak kaliteli ve güvenilir hizmetimizle yanınızdayız.",
+    imgSrc: ETicaretImg,
+    imgAlt: "E-Ticaret Lojistiği",
+  },
+  {
+    title: "Depolama Hizmetleri",
+    description: "Türkiye ve Almanya’da bulunan depolarımızla depo yönetimi süreçlerinize destek oluyoruz.",
+    imgSrc: DepolamaImg,
+    imgAlt: "Depolama Hizmetleri",
+  },
+  {
+    title: "Sprint Taşımacılığı",
+    description: "Avrupa başta olmak üzere dünyadaki birçok noktaya karayolu taşımacılığı hizmeti sunmaktayız.",
+    imgSrc: SprintImg,
+    imgAlt: "Sprint Taşımacılığı",
+  },
+  {
+    title: "Kontrat Lojistiği",
+    description: "Her türlü lojistik çözümünde Kontrat Lojistiği ile yanınızdayız.",
+    imgSrc: KontratImg,
+    imgAlt: "Kontrat Lojistiği",
+  },
+];
